7c54e0355176f439f8e6d124c88348257f0bf5b8
slavalampdev <slavalampdev@gmail.com>
2020-07-16 17:33:38 +0300

    Fixes + env example

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Installation guide:
-1. Run composer instal;
-2. Create and fill .env file;
-3. Change /Config/Database.php according to you database settings
-4. If needed, change /Config/App.php $baseURL value
-5. Run 'php spark migrate'
+1. Clone project from repository;
+2. Run 'composer instal';
+3. Create and fill .env file according to .env.example file;
+4. Change /Config/Database.php according to you database settings;
+5. If needed, change /Config/App.php $baseURL value;
+6. Run 'php spark migrate';
--- app/Views/homepage.php
+++ app/Views/homepage.php
@@ -5,10 +5,14 @@
         <div class="note">
             <p>Welcome to our home page</p>
         </div>
+        <?php if($data): ?>
         <ul class="list-group">
             <?php foreach ($data as $user): ?>
             <li class="list-group-item"><?php echo $user->name ?> - <span class="badge badge-primary badge-pill" ><?php echo $user->email ?></span></li>
             <?php endforeach; ?>
         </ul>
+        <?php else: ?>
+        <h3>There is no user data available yet</h3>
+        <?php endif;?>
     </div>
 </div>
